81e5e114a231fd63ae623477ae32f977bb557edf
Fabio França <f.ragnar@hotmail.com>
2025-03-24 23:41:13 -0300

    Aumentei a dificuldade para descobrir número secreto de 1 a 50

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
+console.log(`O número secreto é: ${numeroSecreto}`);
 let tentativas = 1;
 
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.3});
 }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
