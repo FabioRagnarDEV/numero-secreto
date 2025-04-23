819738015c2b7ee2a6d9c60f80f1baa2c47d6069
Fabio França <f.ragnar@hotmail.com>
2025-04-22 23:04:55 -0300

    Aumentando a dificuldade do jogo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 console.log(`O número secreto é: ${numeroSecreto}`);
 let tentativas = 1;
